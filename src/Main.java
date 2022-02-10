65c9bdcb48165b789086ab8ca098935d41e9ed4a
orlova <orlova.ann.2306@gmail.com>
2022-02-10 21:24:20 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 1000;
+        int mile = 20;
+
+        int bonusMile = ticketPrice / mile;
+
+        System.out.println("Ваши бонусные мили: " + bonusMile);
+
+    }
+}
